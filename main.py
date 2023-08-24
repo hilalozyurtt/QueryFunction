a6ddfb28a11622dc7b5f1ee7740e35d6ce225163
hilalozyurtt <hilalozyurt1256@hotmail.com>
2023-08-24 16:10:12 +0300

    Yorum satırları eklendi.

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from google.cloud import bigquery
 import json
 
 def query_bigquery(request):
-    client = bigquery.Client()
+    # Signs in to Google Cloud
+    client = bigquery.Client() 
 
+    # Prepare your query to pull data from BigQuery
     query = """
         SELECT title
         FROM `bigquery-public-data.bbc_news.fulltext`
@@ -11,16 +13,19 @@ def query_bigquery(request):
         LIMIT 50
     """
 
-    query_job = client.query(query)
-    results = query_job.result()
+    query_job = client.query(query) # API request
+    results = query_job.result() # Waits for query to finish
 
     print(results)
 
+    # A dictionary is created to store the results in a JSON format
     data = []
     for row in results:
         data.append({"title":row.title})
 
+    # Results are returned in JSON format
     json_data = json.dumps(data)
     
+    # HTTP response is generated for Cloud Function
     headers = {'Content-Type': 'application/json'}
     return (json_data, 200, headers)
